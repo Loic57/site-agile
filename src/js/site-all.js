2dc3670e005fe5d646be979be5c4ee1d32d7a790
loic57 <l.sciampagna@live.fr>
2019-01-11 06:30:20 +0100

    t

--- src/js/site-all.js
+++ src/js/site-all.js
@@ -167,4 +167,41 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   /********** component tabs **********/
 
+
+
+  var getOffsetTop = function (elem) {
+  	// Set our distance placeholder
+  	var distance = 0;
+
+  	// Loop up the DOM
+  	if (elem.offsetParent) {
+  		do {
+  			distance += elem.offsetTop;
+  			elem = elem.offsetParent;
+  		} while (elem);
+  	}
+
+  	// Return our distance
+  	return distance < 0 ? 0 : distance;
+  };
+
+
+  const scroll = document.querySelector('#scroll');
+  const fixedElement = document.querySelector('#scroll .column.w-30 .wrapper');
+  const offset = getOffsetTop(scroll);
+
+  window.addEventListener('scroll', function() {
+    let scrollTop = this.scrollY;
+
+    if(scrollTop > offset - 50) {
+      scroll.classList.add('is-fixed');
+      if(window.innerWidth > 1024) {
+        fixedElement.style.top = scrollTop - 275 + "px";
+      }
+    }
+    else {
+      scroll.classList.remove('is-fixed');
+    }
+  });
+
 }, false);
